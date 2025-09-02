e3c9f5d10a459c0e87f42c6d8c060cdb480c1229
Piyush Raj <piyushraj102000@gmail.com>
2025-09-02 23:49:22 +0530

    Day 04: Integrated Monaco code editor and built admin problem creation panel

--- backend/src/controllers/userSubmission.js
+++ backend/src/controllers/userSubmission.js
@@ -9,10 +9,13 @@ const submitCode = async(req,res)=>{
         const userId = req.result._id; //from user middleware
         const problemId = req.params.id;
 
-        const {code,language} = req.body;
+        let {code,language} = req.body;
         
         if(!userId || !code || !problemId || !language)
             return res.status(400).send("some field missing");
+
+         if(language==='cpp')
+        language='c++'
 
         //  fetch the problem from DB
         const problem  = await Problem.findById(problemId);
@@ -77,6 +80,7 @@ const submitCode = async(req,res)=>{
                 else
                 {
                     status="wrong";
+                    errorMessage = test.stderr
                 } 
             }
 
@@ -98,11 +102,15 @@ const submitCode = async(req,res)=>{
             req.result.problemSolved.push(problemId);
             await req.result.save();
         }
-
-        res.status(201).send(submittedResult);
-
-
-
+       const accepted  = (status == 'accepted');
+        res.status(201).
+        json({
+            accepted,
+            totalTestCases : submittedResult.testCasesTotal,
+            passedTestCases: testCasesPassed,
+            runtime,
+            memory
+        });
 
     }
     catch (err) {
@@ -113,53 +121,77 @@ const submitCode = async(req,res)=>{
     }
 }
 
-const runCode = async(req,res)=>
-{
-   try {
+const runCode = async(req,res)=>{
     
-        const userId = req.result._id; 
-        const problemId = req.params.id;
+     // 
+     try{
+      const userId = req.result._id;
+      const problemId = req.params.id;
 
-        const {code,language} = req.body;
-        
-        if(!userId || !code || !problemId || !language)
-            return res.status(400).send("some field missing");
+      let {code,language} = req.body;
 
-        const problem  = await Problem.findById(problemId);
+     if(!userId||!code||!problemId||!language)
+       return res.status(400).send("Some field missing");
 
-        if (!problem) {
-         return res.status(404).json({ error: "Problem not found" });
+   //    Fetch the problem from database
+      const problem =  await Problem.findById(problemId);
+   //    testcases(Hidden)
+      if(language==='cpp')
+        language='c++'
+
+
+   const languageId = getLanguageById(language);
+
+   const submissions = problem.visibleTestCases.map((testcase)=>({
+       source_code:code,
+       language_id: languageId,
+       stdin: testcase.input,
+       expected_output: testcase.output
+   }));
+
+
+   const submitResult = await submitBatch(submissions);
+   
+   const resultToken = submitResult.map((value)=> value.token);
+
+   const testResult = await submitToken(resultToken);
+
+    let testCasesPassed = 0;
+    let runtime = 0;
+    let memory = 0;
+    let status = true;
+    let errorMessage = null;
+
+    for(const test of testResult){
+        if(test.status_id==3){
+           testCasesPassed++;
+           runtime = runtime+parseFloat(test.time)
+           memory = Math.max(memory,test.memory);
+        }else{
+          if(test.status_id==4){
+            status = false
+            errorMessage = test.stderr
+          }
+          else{
+            status = false
+            errorMessage = test.stderr
+          }
         }
- 
-       //submit code to judge0
-
-       const languageId = getLanguageById(language);
-
-
-        //only visible test cases
-       const submissions = problem.visibleTestCases.map((testcase)=>({
-            source_code:code,
-            language_id: languageId,
-            stdin: testcase.input,
-            expected_output: testcase.output
-        }));
-
-        const submitResult = await submitBatch(submissions);
-
-        const resultToken = submitResult.map((value)=> value.token);
-
-        const testResult = await submitToken(resultToken);
-
-    
-        res.status(201).send(testResult);
-
     }
-    catch (err) {
-     
-        console.error("Error in submitCode:", err);
-        res.status(500).send("Internal Server Error");
 
-    }
+   
+  
+   res.status(201).json({
+    success:status,
+    testCases: testResult,
+    runtime,
+    memory
+   });
+      
+   }
+   catch(err){
+     res.status(500).send("Internal Server Error "+ err);
+   }
 }
 
 module.exports ={submitCode,runCode};
